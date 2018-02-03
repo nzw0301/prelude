d7fc074c1387af9ec3b53dd2b32094e4be7b5ee6
Kento NOZAWA <k_nzw@klis.tsukuba.ac.jp>
2018-02-03 22:11:23 +0900

    Add hunspell setting

--- personal/settings.el
+++ personal/settings.el
@@ -2,6 +2,25 @@
 (scroll-bar-mode 0) ;; remove scroll bar
 (latex-preview-pane-enable)
 (turn-off-auto-fill)
+(setenv "DICTIONARY" "en_US")
+
+
+(cond
+ ;; try hunspell at first
+ ;; if hunspell does NOT exist, use aspell
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary-alist
+        ;; Please note the list `("-d" "en_US")` contains ACTUAL parameters passed to hunspell
+        ;; You could use `("-d" "en_US,en_US-med")` to check with multiple dictionaries
+        '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US") nil utf-8)
+          )))
+
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  ;; Please note ispell-extra-args contains ACTUAL parameters passed to aspell
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US"))))
 
 ;; font settings
 (add-to-list 'default-frame-alist '(font . "Ricty Diminished Discord-18"))
